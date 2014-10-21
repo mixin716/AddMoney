370f22fb12e3f5d23ccf1fecdb0de304997f57c3
mixin105 <2649964929@qq.com>
2014-10-21 08:50:36 +0800

    Main

--- src/com/zc/addmony/MainActivity.java
+++ src/com/zc/addmony/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private String str;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
